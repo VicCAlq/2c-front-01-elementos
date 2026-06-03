6bf37c11922ae80d451401e1aa6819438d293dee
Andrey Vieira <andreyantonio2020@gmail.com>
2026-06-03 22:43:51 +0000

    AtvBotaoAcaoInterna Feita

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -1,5 +1,20 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
+Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos:
+ um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
+export default function AtvBotaoAcaoInterna ({}) {
+    return (
+
+        <div>
+        <p>Clique abaixo</p>
+        <button
+        style={{
+            color: "Black",
+            borderRadius: "50px"
+        }}
+        onClick={ () => alert ("Fui clicado")}>Clique aqui</button>
+        </div>
+    )
+}
