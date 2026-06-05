cca6d07c95b264543c065adefb5ca0d379e7fc3d
Renato-Marques-Nave <renato.marques@nave.org.br>
2026-06-05 09:51:05 -0300

    oiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -3,3 +3,18 @@
 
 Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
+
+
+export default function AtvBotaoAcaoInterna() {
+  
+    function click() {
+        alert("cricaru!");
+    }
+    
+    return (
+        <div>
+            <p>Clique abaixo</p>
+            <button onClick={click}>Clique aqui</button>
+        </div>
+    );
+}
