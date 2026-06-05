e976da52c82e37b989ae907b27d84922a6e03de9
yan-arthur-nave <yan.arthur@nave.org.br>
2026-06-05 13:22:46 +0000

    comitei :-D (eu gosto de sonic, entao eh sobre aneis)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 import Divisoria from './components/Divisoria';
 import Paragrafo from './components/Paragrafo';
 import Botao from './components/Botao';
+import AtvBotaoAcaoExterna from './components/AtvBotaoAcaoExterna';
+import AtvBotaoAcaoInterna from './components/AtvBotaoAcaoInterna';
+import AtvParagrafo from './components/AtvParagrafo';
+import AtvDivisoria from './components/AtvDivisoria';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +17,10 @@ function App() {
         <Divisoria>
           <Paragrafo conteudo="Sou um exemplo"/>
           <Botao/>
+          <AtvBotaoAcaoExterna/>
+          <AtvBotaoAcaoInterna/>
+          <AtvParagrafo/>
+          <AtvDivisoria/>
         </Divisoria>
       </section>
     </>
--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -1,5 +1,17 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
+Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento.
+ Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um
+  <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
+
+export default function AtvBotaoAcaoInterna(){
+    return (<div>
+<p>
+    "Clique abaixo!!!!!1"
+</p>
+<button onClick={() => alert ("Você acabou de ganhar 100 anéis de graça, parabéns! :-)")}>"Clique aki para ganhar ANÉIS!!!!!"</button>
+    </div>)
+
+}
