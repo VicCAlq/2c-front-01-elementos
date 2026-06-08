ddf7079c7ecac25c1d07b4e5cfbbe7b491632ff8
Isaac-Andrade-Santos-NAVE <isaac.andrade@nave.org.br>
2026-06-08 12:00:12 -0300

    envio parcial

--- src/components/AtvBotaoAcaoExterna.jsx
+++ src/components/AtvBotaoAcaoExterna.jsx
@@ -1,5 +1,28 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO EXTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
+Crie e exporte por padrão um componente chamado "AtvBotaoAcaoExterna" que recebe como argumento uma função chamada "comando". 
+Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", 
+e um <button> com o texto "Clique aqui" que ao ser pressionado executa a função recebida no argumento "comando".
 */
+
+const estilo = {
+    botao: {
+        borderRadius: "5px",
+        padding: root.spacings.paddingM,
+        backgroundColor: root.colors.primaria,
+    },
+    texto: {
+        color: root.colors.texto,
+        fontSize: "16px",
+    }
+}
+
+export default function AtvBotaoAcaoExterna({ comando }) {
+    return (
+        <div>
+            <p style={ estilo.texto }></p>
+            <button onClick={ comando } style={ estilo.botao }>Clique aqui</button>
+        </div>
+    )
+}
--- src/components/AtvBotaoAcaoInterna.jsx
+++ src/components/AtvBotaoAcaoInterna.jsx
@@ -1,5 +1,28 @@
 /*
 [ATIVIDADE BOTÃO COM AÇÃO INTERNA]
 
-Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
+Crie e exporte por padrão um componente chamado "AtvBotaoAcaoInterna" que não recebe nenhum argumento. 
+Este componente deve exibir uma <div> com dois elementos: um <p> com o texto "Clique abaixo", 
+e um <button> com o texto "Clique aqui" que ao ser clicado exibe um alerta com o texto "Fui clicado!"
 */
+
+const estilo = {
+    botao: {
+        borderRadius: "5px",
+        padding: root.spacings.paddingM,
+        backgroundColor: root.colors.primaria,
+    },
+    texto: {
+        color: root.colors.texto,
+        fontSize: "16px",
+    }
+}
+
+export default function AtvBotaoAcaoInterna() {
+    return (
+        <div>
+            <p style={ estilo.texto }></p>
+            <button onClick={ () => console.log("Fui clicado!") } style={ estilo.botao }>Clique aqui</button>
+        </div>
+    )
+}
