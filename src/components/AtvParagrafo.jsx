33fe48f4ef02d9569fa9f47dc8958506fb8b46d6
Wally1542 <walleysson.ferreira@nave.org.br>
2026-06-08 11:45:42 -0300

    Update AtvParagrafo.jsx

--- src/components/AtvParagrafo.jsx
+++ src/components/AtvParagrafo.jsx
@@ -1,5 +1,28 @@
+import { Children } from "react";
+
 /*
 [ATIVIDADE PARÁGRAFO]
 
-Crie e exporte por padrão um componente chamado "AtvParagrafo" que recebe dois argumentos: um "titulo" e um "conteudo". O componente deve exibir uma <div> com um elemento <h1> e um elemento <p> internos. O <h1> terá o conteúdo do argumento "título" e <p> terá o conteúdo do argumento "conteudo". O <h1> de "titulo" deve ter o tamanho de 32px, e o <p> de "conteudo" deve ter o tamanho de 16px. O texto do "titulo" deve ficar acima do texto do "conteudo".
+Crie e exporte por padrão um componente chamado "AtvParagrafo" que recebe dois argumentos: um "titulo" e um "conteudo". 
+O componente deve exibir uma <div> com um elemento <h1> e um elemento <p> internos.  
+O <h1> terá o conteúdo do argumento "título" e <p> terá o conteúdo do argumento "conteudo".  
+O <h1> de "titulo" deve ter o tamanho de 32px, e o <p> de "conteudo" deve ter o tamanho de 16px.  
+O texto do "titulo" deve ficar acima do texto do "conteudo".
 */
+const estilo = {
+    titulo: {
+        fontSize: '32px'
+    },
+    conteudo: {
+        fontSize: '16px',
+    }
+};
+
+export default function AtvParagrafo({ t, c }) {
+  return (
+    <div>
+      <h1 style={estilo.titulo}>{t}</h1>
+      <p style={estilo.conteudo}>{c}</p>
+    </div>
+  );
+}
